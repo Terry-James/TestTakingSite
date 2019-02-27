0cc1dc584be565269ae4a9650c8b90a1d8ecf043
Terry-James <37746020+Terry-James@users.noreply.github.com>
2019-02-27 14:42:58 -0600

    inital

---
+++ javascript/myScript.js
@@ -0,0 +1,9 @@
+"use strict"
+
+window.load = function(){
+    var quizContainer = document.getElementById('question');
+    var quizContainer = document.getElementById('answer');
+    var submitButton = document.getElementById('submit');
+}
+
+
