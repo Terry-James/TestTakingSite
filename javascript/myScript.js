7347a89d478a30b43aba7268fc600d3cbdc1c7f5
Terry-James <37746020+Terry-James@users.noreply.github.com>
2019-02-27 19:12:23 -0600

    added javascript function headers

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -1,9 +1,50 @@
 "use strict"
 
 window.load = function(){
-    var quizContainer = document.getElementById('question');
+    
     var quizContainer = document.getElementById('answer');
     var submitButton = document.getElementById('submit');
+    var allQuestions = [];
+    var userAnswers = [];
+}
+var quizContainer = document.getElementById('question');
+addQuestion();
+function addQuestion(){
+    console.log(quizContainer);
 }
 
+function deleteQuestion(){
 
+}
+
+function updateQuestion(){
+
+}
+
+function buildQuiz(){
+
+}
+
+function randomizeQuiz(){
+
+}
+
+function showInstanceFeedback(){
+
+}
+
+function showAllResults(){
+
+}
+
+function showNextQuestion(){
+
+}
+
+function showPreviousQuestion(){
+
+}
+
+function showStatusBar(){
+
+}
