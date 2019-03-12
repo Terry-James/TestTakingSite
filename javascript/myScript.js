5c0b01fcb9d0383d80f9bf13fa8bd0d4c6bce95f
Terry-James <37746020+Terry-James@users.noreply.github.com>
2019-03-12 18:34:48 -0500

    combine html pages into one/ updated javascript file

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -1,6 +1,9 @@
 "use strict"
 
-window.onload = function(){
+window.onload = function () {
+    document.getElementById('testNav').style.display = "none";
+    document.getElementById('testingDiv').style.display = "none";
+    document.getElementById('adminNav').style.display = "none";
     document.getElementById('multi').style.display = "none";
     document.getElementById('selection').style.display = "none";
     document.getElementById('multAns').style.display = "none";
@@ -11,24 +14,41 @@ window.onload = function(){
     document.getElementById('question');
     document.getElementById('answer');
     document.getElementById('submit');
-     
+
     document.getElementById("multiButton").onclick = addingMultiChoice;
     document.getElementById("answerButton").onclick = addingMultiAnswer;
     document.getElementById("trueButton").onclick = addingTrueFalse;
     document.getElementById("fillButton").onclick = addingFillBlank;
     document.getElementById("shortButton").onclick = addingShortAnswer;
-    document.getElementById("home").onclick = returnHome;
-    
+    document.getElementById("testTaker").onclick = testLayout;
+    document.getElementById("adminButton").onclick = adminLayout;
+    var homePage = document.getElementsByClassName('home');
+    homePage[0].onclick = returnHome;
+    homePage[1].onclick = returnHome;
+
     var allQuestions = [];
     var userAnswers = [];
     var answerStatus = []; // (boolean array) should be the size of allQuestion array
 }
 
-function returnHome(){
+function testLayout() {
+    document.getElementById('opener').style.display = "none";
+    document.getElementById('testNav').style.display = "block";
+    document.getElementById('testingDiv').style.display = "block";
+
+}
+
+function adminLayout() {
+    document.getElementById('opener').style.display = "none";
+    document.getElementById('adminNav').style.display = "block";
+
+}
+
+function returnHome() {
     location = "index.html";
 }
 
-function addQuestion(){
+function addQuestion() {
     document.getElementById('multi').style.display = "none";
     document.getElementById('selection').style.display = "none";
     document.getElementById('multAns').style.display = "none";
@@ -38,64 +58,64 @@ function addQuestion(){
     document.getElementById('selection').style.display = "block";
 }
 
-function addingMultiChoice(){
-    document.getElementById('selection').style.display = "none";   
+function addingMultiChoice() {
+    document.getElementById('selection').style.display = "none";
     document.getElementById('multi').style.display = "block";
 
 }
 
-function addingMultiAnswer(){
-    document.getElementById('selection').style.display = "none";   
+function addingMultiAnswer() {
+    document.getElementById('selection').style.display = "none";
     document.getElementById('multAns').style.display = "block";
 }
 
-function addingTrueFalse(){
-    document.getElementById('selection').style.display = "none";   
+function addingTrueFalse() {
+    document.getElementById('selection').style.display = "none";
     document.getElementById('trueOrFalse').style.display = "block";
 }
 
-function addingFillBlank(){
-    document.getElementById('selection').style.display = "none";   
+function addingFillBlank() {
+    document.getElementById('selection').style.display = "none";
     document.getElementById('fillBlank').style.display = "block";
 }
 
-function addingShortAnswer(){
-    document.getElementById('selection').style.display = "none";   
+function addingShortAnswer() {
+    document.getElementById('selection').style.display = "none";
     document.getElementById('shortAnswer').style.display = "block";
 }
 
-function deleteQuestion(){
-    
-}
-
-function updateQuestion(){
+function deleteQuestion() {
 
 }
 
-function buildQuiz(){
+function updateQuestion() {
 
 }
 
-function randomizeQuiz(){
+function buildQuiz() {
 
 }
 
-function showInstanceFeedback(){
+function randomizeQuiz() {
 
 }
 
-function showAllResults(){
+function showInstanceFeedback() {
 
 }
 
-function showNextQuestion(){
+function showAllResults() {
 
 }
 
-function showPreviousQuestion(){
+function showNextQuestion() {
 
 }
 
-function showStatusBar(){
+function showPreviousQuestion() {
+
+}
+
+function showStatusBar() {
 
 }
